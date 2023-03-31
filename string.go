2e5229a0b7666a727f19fb0488ada4574b44324b
Faiz <hairrichtalk@gmail.com>
2023-03-31 09:35:13 +0700

    add new file string.go

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Berseker")
+	fmt.Println("Blade")
+
+	//know count character
+	fmt.Println(len("Berseker"))
+
+	//know index bit
+	fmt.Println("Blade"[1])
+}
